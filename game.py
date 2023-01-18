e3950c78024fd5329bd16b40e3bb3146e7396724
Muntaha Tahiat <107261746+tahi4@users.noreply.github.com>
2023-01-18 21:30:57 +0600

    Add files via upload

---
+++ game.py
@@ -0,0 +1,25 @@
+import random 
+
+# conditions true, player wins
+def player_wins(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p'):
+        return True
+
+
+def game():
+    # choosing
+    user = input("'r' for rock, 'p' for paper, 's' for scissors: ").lower()
+    pc = random.choice(['r', 's', 'p'])
+
+    choices = f'You gave {user}, and PC gave {pc}'
+    
+    # deciding results
+    if user == pc:
+        return f"{choices}, It's a tie :/"
+    if player_wins(user, pc):
+        return f"{choices}, You won :)"
+    return f"{choices}, You Lost :("
+    
+
+
+print(game())
