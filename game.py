d4a98f29d6d1340f8c107789234c5e13ace9641b
Muntaha Tahiat <107261746+tahi4@users.noreply.github.com>
2023-01-19 22:39:04 +0600

    Update game.py

--- game.py
+++ game.py
@@ -6,10 +6,11 @@ def player_wins(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p'):
         return True
 
-
+#
 def game():
     # choosing
     user = input("'r' for rock, 'p' for paper, 's' for scissors: ").lower()
+    # random.choice lets you pick from array randomly
     pc = random.choice(['r', 's', 'p'])
 
     choices = f'You gave {user}, and PC gave {pc}'
